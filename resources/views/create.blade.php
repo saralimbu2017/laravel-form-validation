5e63bcf462274152c7b73726f4de1b56855f3453
saralimbu2017 <iamsebi7@gmail.com>
2021-12-07 13:29:03 +1100

    Creating html form at create view

--- resources/views/create.blade.php
+++ resources/views/create.blade.php
@@ -1,12 +1,9 @@
-<!DOCTYPE html>
-<html lang="en">
-<head>
-  <meta charset="UTF-8">
-  <meta http-equiv="X-UA-Compatible" content="IE=edge">
-  <meta name="viewport" content="width=device-width, initial-scale=1.0">
-  <title>Document</title>
-</head>
-<body>
-  
-</body>
-</html>
+@extends('layouts.app')
+
+@section('content')
+  <form method="post" action="/posts">
+    <input type="text" name="title" placeholder="Enter title">
+    <input type="submit" name="submit">
+  </form>
+
+@yield('footer')
