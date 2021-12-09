6e9af17e071ff7cd0ea7271835496b87deb9137d
saralimbu2017 <iamsebi7@gmail.com>
2021-12-09 22:46:15 +1100

    Fixing typos

--- database/migrations/2021_12_06_022620_create_posts_table.php
+++ database/migrations/2021_12_06_022620_create_posts_table.php
@@ -15,7 +15,7 @@ class CreatePostsTable extends Migration
     {
         Schema::create('posts', function (Blueprint $table) {
             $table->id();
-            $table->integer('user_id')->unsigned();
+            $table->integer('user_id');
             $table->string('title');
             $table->text('content');
             $table->timestamps();
