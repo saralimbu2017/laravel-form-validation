98e7d3c930f336d82ed91170fc27150dd92486ce
saralimbu2017 <iamsebi7@gmail.com>
2021-12-09 20:15:17 +1100

    Creating create.blade in views/posts folder

---
+++ resources/views/posts/create.blade.php
@@ -0,0 +1,10 @@
+@extends('layouts.app')
+
+@section('content')
+  <form method="post" action="/posts">
+    @csrf 
+    <input type="text" name="title" placeholder="Enter title">
+    <input type="submit" name="submit">
+  </form>
+
+@yield('footer')
