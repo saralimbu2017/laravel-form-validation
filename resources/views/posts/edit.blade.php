f67122f706ded964aa3e5d333015cae5b29b7992
saralimbu2017 <iamsebi7@gmail.com>
2021-12-10 12:50:05 +1100

    Adding form for delete

--- resources/views/posts/edit.blade.php
+++ resources/views/posts/edit.blade.php
@@ -9,4 +9,10 @@
     <input type="text" name="title" placeholder="Enter title" value="{{$post->title}}">
     <input type="submit" name="submit">
   </form>
+
+  <form method="post" action="/posts/{{$post->id}}">
+    {{csrf_field()}} 
+    <input type="hidden" name="_method" value="DELETE">
+    <input type="submit" name="DELETE" value = "DELETE">
+  </form>
 @endsection
