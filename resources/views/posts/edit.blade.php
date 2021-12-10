6ead6ac60b5750c2180783dda61520b665ce082a
saralimbu2017 <iamsebi7@gmail.com>
2021-12-10 12:25:47 +1100

    Declaring PUT method for update

--- resources/views/posts/edit.blade.php
+++ resources/views/posts/edit.blade.php
@@ -3,9 +3,10 @@
 @section('content')
 
   <h1>Edit Post</h1>
-  <form method="post" action="/posts">
-    @csrf 
-    <input type="text" name="title" placeholder="Enter title">
+  <form method="post" action="/posts/{{$post->id}}">
+    {{csrf_field()}} 
+    <input type="hidden" name="_method" value="PUT">
+    <input type="text" name="title" placeholder="Enter title" value="{{$post->title}}">
     <input type="submit" name="submit">
   </form>
 @endsection
