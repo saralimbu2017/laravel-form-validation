23522ee6720fccd1cde89f5f1ada7680fd5bb92f
saralimbu2017 <iamsebi7@gmail.com>
2021-12-10 12:09:01 +1100

    creating edit view

---
+++ resources/views/posts/edit.blade.php
@@ -0,0 +1,11 @@
+@extends('layouts.app')
+
+@section('content')
+
+  <h1>Edit Post</h1>
+  <form method="post" action="/posts">
+    @csrf 
+    <input type="text" name="title" placeholder="Enter title">
+    <input type="submit" name="submit">
+  </form>
+@endsection
