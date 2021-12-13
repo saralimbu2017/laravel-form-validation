78203bd081787efaa4be866d9336e74fd9a3e406
saralimbu2017 <iamsebi7@gmail.com>
2021-12-13 11:05:09 +1100

    Adding illuminate html package and editing create html form

--- resources/views/posts/create.blade.php
+++ resources/views/posts/create.blade.php
@@ -1,10 +1,12 @@
 @extends('layouts.app')
 
 @section('content')
-  <form method="post" action="/posts">
+  <h1>Create Post</h1>
+  <!-- <form method="post" action="/posts"> -->
+  {!! Form::open() !!}
     @csrf 
     <input type="text" name="title" placeholder="Enter title">
     <input type="submit" name="submit">
-  </form>
+  <!-- </form> -->
 
 @endsection
