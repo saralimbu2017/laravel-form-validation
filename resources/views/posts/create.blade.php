751a317a5a349949276724a5fad5a84c6dce5b56
saralimbu2017 <iamsebi7@gmail.com>
2021-12-13 11:41:50 +1100

    Adding route method to the create form

--- resources/views/posts/create.blade.php
+++ resources/views/posts/create.blade.php
@@ -3,7 +3,7 @@
 @section('content')
   <h1>Create Post</h1>
   <!-- <form method="post" action="/posts"> -->
-  {!! Form::open() !!}
+  {!! Form::open(['method' => 'POST','route' => 'posts.store']) !!}
     @csrf 
     <input type="text" name="title" placeholder="Enter title">
     <input type="submit" name="submit">
